9c362ca1185f93a8976889b1bde7afea396ff820
llmthpanll <methasitpan@gmail.com>
2022-07-12 00:21:20 +0700

    .

--- Term_1/test.py
+++ Term_1/test.py
@@ -1,9 +1,18 @@
 """test"""
 def main():
     """test"""
+    my_ans=[]
     txt = input("Enter 2 imtegers : ").split()
-    for i in range(int(txt[0]), int(txt[1])):
-        if i%2==1:
-            break
-    print(txt)
+    txt = sorted(txt)
+    if txt[0].isdigit() == False or txt[1].isdigit() == False:
+        print("Invalid input !!!")
+    else:
+        num_min = min(int(txt[0]), int(txt[1]))
+        num_max = max(int(txt[0]), int(txt[1]))
+        for i in range(num_min, num_max+1):
+            if i%2==1:
+                my_ans.append(i)
+        for j in range(len(my_ans)):
+            ans = str(my_ans).replace(", ", "+").replace("[", "").replace("]", "")
+        print("%s = %d" %(ans, (sum(my_ans))))
 main()
